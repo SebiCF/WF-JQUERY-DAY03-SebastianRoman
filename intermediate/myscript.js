d71b8f8018a611861b8cd179917f1d7468b1e588
roman.cf <roman.springer@outlook.com>
2019-10-23 14:14:55 +0200

    alles fertig - viel zu früh

--- intermediate/myscript.js
+++ intermediate/myscript.js
@@ -11,3 +11,35 @@
         $(".tablet .product-image").css("background-color","blue");
         
     });
+
+
+arrSelect=["all","laptop","mobile","tablet"];
+
+let dropDown = document.createElement("select");
+	dropDown.addEventListener("change",remover,false);
+	document.getElementsByTagName("nav")[0].appendChild(dropDown);
+		for (let i=0; i<arrSelect.length;i++){
+			var option = document.createElement("option");
+				option.value = arrSelect[i];
+				option.textContent = arrSelect[i];
+				dropDown.appendChild(option);
+		}
+
+
+function remover(){
+	if ($("nav>select").val()=="all"){
+		$(".hidden").css("display","inline-block").removeClass("hidden")
+		;
+	}else{
+		$(".hidden").css("display","inline-block").removeClass("hidden")
+
+		for (let i=1; i<arrSelect.length;i++){
+			if (arrSelect[i]!=$("nav>select").val()){
+				$("."+arrSelect[i]).css("display","none")
+				$("."+arrSelect[i]).addClass("hidden")
+			}
+		}		
+
+	}
+
+}
