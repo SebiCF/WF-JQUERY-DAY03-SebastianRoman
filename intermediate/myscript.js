dbc3e8e004880603145d666a3dc34244ad5ce6a2
roman.cf <roman.springer@outlook.com>
2019-10-23 11:15:12 +0200

    intermediate fertig

---
+++ intermediate/myscript.js
@@ -0,0 +1,13 @@
+ // $(document).ready(function() {
+ //        $(".product-image:lt(2)").css("background-color","darkred");
+ //        $(".product-image:gt(1)").css("background-color","green");
+ //        $(".product-image:gt(3)").css("background-color","blue");
+        
+ //    });
+
+ $(document).ready(function() {
+        $(".laptop .product-image").css("background-color","darkred");
+        $(".mobile .product-image").css("background-color","green");
+        $(".tablet .product-image").css("background-color","blue");
+        
+    });
